cf43e3e50196f8fb92ed784447fd72ca43312fa9
ValeryEgorova <leraeremeyeva@gmail.com>
2023-11-19 18:17:44 +0300

    []

---
+++ README.md
@@ -0,0 +1,5 @@
+# **Students' involvement in research activities: environmental and psychological factors**
+
+This repository contains codes needed to reproduce the analysis behind the paper [paper title] by [paper authors] published in [journal name].
+
+The study is available via the following [link].
---
+++ user_profile.R
@@ -0,0 +1,50 @@
+#-------------------------------------------------------------------
+# Project: Scientific Involvement Index
+# Organization: SFedU Future Skills Research Lab
+# Author: Valeria Egorova
+# Date: 19 Nov 2023
+#-------------------------------------------------------------------
+
+
+# set working directories and all directories 
+# this is the profile for the Scientific Involvement Index research project
+# this profile should be loaded before running any other script
+
+# for Windows
+USERNAME    <- Sys.getenv("USERNAME")
+# for Mac
+USER        <- Sys.getenv("USER")
+
+#version from everyone, the profile works for everyone
+
+if (USERNAME == "kavanesyan"){
+  projectFolder  <- getwd()
+} 
+
+if (USERNAME == "Valery"){
+  projectFolder  <- getwd()
+} 
+
+if (USER == "karavan88"){
+  projectFolder  <- getwd()
+} 
+
+# confirm that the main directory is correct
+# check if the folders exist
+stopifnot(dir.exists(projectFolder))
+
+# set up key folders
+documentation <-  file.path(projectFolder, "00_documentation")
+inputData     <-  file.path(projectFolder, "01_input_data")
+rcodes        <-  file.path(projectFolder, "02_codes")
+outData       <-  file.path(projectFolder, "03_outputs/0301_data")
+outTables     <-  file.path(projectFolder, "03_outputs/0302_tables")  
+outFigures    <-  file.path(projectFolder, "03_outputs/0303_figures")  
+
+
+stopifnot(dir.exists(documentation))
+stopifnot(dir.exists(inputData))
+stopifnot(dir.exists(rcodes))
+stopifnot(dir.exists(outData))
+stopifnot(dir.exists(outTables))
+stopifnot(dir.exists(outFigures))
