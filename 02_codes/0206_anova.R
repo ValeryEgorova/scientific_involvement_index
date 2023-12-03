778d7b910986c1a9bc7a504fc6ecf7a9aaf0df34
ValeryEgorova <leraeremeyeva@gmail.com>
2023-12-03 23:29:15 +0300

    [code]

--- 02_codes/0206_anova.R
+++ 02_codes/0206_anova.R
@@ -12,6 +12,13 @@ data_high <-
   data_for_glm%>%
   filter(group == 1)%>%
   mutate(sc_type = as.factor(sc_type))%>%
+  select(sc_type, group, self_control, self_efficacy, educational,
+         communicative, emotional, growth, creativity,
+         position, achievment, external, self_respect, motivational, 
+         cognitive, behavioural, research_potential, em_knowledge,
+         em_control, self_mot, empaty, em_identification,
+         O, C, E, A, N, gm_intelligence, gm_personality, 
+         externalism,  commitment, consistency, grit)%>%
   drop_na()
 
 #####---------ANOVA_Schools--------######
@@ -26,7 +33,7 @@ var2 <- c("self_control", "self_efficacy", "educational",
 sum_aov<- list()
 Tukey <- list()
 
-for (i in 1:length(var)) {
+for (i in 1:length(var2)) {
   
   formula <- as.formula(paste0(var2[i], "  ~  data_high$sc_type"))
   sum_aov[[i]] <- aov(formula, data = data_high)
